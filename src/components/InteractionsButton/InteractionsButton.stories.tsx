fc90da82118f142f95c79a159ee38ddbd9d9a43a
Mikko Luhtasaari <mikko.luhtasaari@gmail.com>
2023-02-07 09:24:54 +0200

    Add more instructions

--- src/components/InteractionsButton/InteractionsButton.stories.tsx
+++ src/components/InteractionsButton/InteractionsButton.stories.tsx
@@ -14,9 +14,10 @@ export default {
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof InteractionsButton> = (args) => <InteractionsButton {...args} />;
 
+const textAfterClicking = "Clicked";
 const staticArgs = {
   initialLabel: "Not clicked",
-  afterClickLabel: "Clicked",
+  afterClickLabel: textAfterClicking,
 };
 
 export const WithoutInteractions = Template.bind({});
@@ -26,16 +27,17 @@ WithoutInteractions.args = staticArgs;
 export const Example1 = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Example1.args = staticArgs;
+// More about play: https://storybook.js.org/docs/react/essentials/interactions
 Example1.play = async ({ canvasElement }) => {
+  // More about canvas: https://storybook.js.org/docs/react/writing-stories/play-function#working-with-the-canvas
   const canvas = within(canvasElement);
 
   await userEvent.click(canvas.getByTestId("button-to-click"));
 
-  await expect(canvas.getByText("Clicked")).toBeInTheDocument();
+  await expect(canvas.getByText(textAfterClicking)).toBeInTheDocument();
 };
 
 export const Example2 = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Example2.args = staticArgs;
 Example2.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement);
